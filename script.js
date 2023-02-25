445a070ce6c152a107cc64c1e04320c2ad2b9cf5
RogerioLacerdaGH <nenorogerinho@gmail.com>
2023-02-24 21:50:00 -0300

    modificações do display

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const numberButtons = document.querySelectorAll('[data-number]')
 const operationButtons = document.querySelectorAll('[data-operator]')
 const equalsButton = document.querySelector('[data-equals]')
 const deleteButton = document.querySelector('[data-delete]')
-const allButton = document.querySelector('[data-all-clear]')
+const allClearButton = document.querySelector('[data-all-clear]')
 const previousOperandTextElement = document.querySelector('[data-previous-operand]')
 const currentOperandTextElement = document.querySelector('[data-current-operand]')
 
@@ -10,6 +10,22 @@ class Calculator {
     constructor(previousOperandTextElement, currentOperandTextElement) {
         this.previousOperandTextElement = previousOperandTextElement;
         this.currentOperandTextElement = currentOperandTextElement;
+        this.clear();
+    }
+
+    chooseOperation(operation){
+        this.operation = operation;
+
+        this.previousOperand = this.currentOperand;
+        this.currentOperand = '';
+    }
+
+    appendNumber(number){
+        if (this.currentOperand.includes('.') && this.currentOperand[0] == '.') {
+            this.currentOperand = "0.";
+        }
+        if (this.currentOperand.includes('.') && number == '.') return;
+        this.currentOperand = `${this.currentOperand}${number.toString()}`;
     }
 
     clear (){
@@ -29,7 +45,21 @@ const calculator = new Calculator(
     currentOperandTextElement
 );
 
+for(const numberButton of numberButtons){
+    numberButton.addEventListener('click', () => {
+        calculator.appendNumber(numberButton.innerText);
+        calculator.updateDisplay();
+    })
+}
+
+for(const operationButton of operationButtons ){
+    operationButton.addEventListener('click', ()=> {
+        calculator.chooseOperation(operationButton.innerText);
+        calculator.updateDisplay();
+    })
+}
+
 allClearButton.addEventListener('click', () => {
     calculator.clear();
-    this.updateDisplay();
-})
+    calculator.updateDisplay();
+}) 
